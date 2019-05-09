52825caece742beeb6ec5579709dce8bccc6fef5
Jacknic <jacknic@qq.com>
2019-05-09 14:41:21 +0800

    简化代码

--- app/src/main/java/com/jacknic/circleci/MainActivity.kt
+++ app/src/main/java/com/jacknic/circleci/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.jacknic.circleci
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvHello = findViewById<TextView>(R.id.tvHello)
         val versionName = getString(R.string.version_format, BuildConfig.VERSION_NAME)
         tvHello.append(versionName)
     }
